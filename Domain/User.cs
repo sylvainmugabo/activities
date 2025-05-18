9b911711d8e64327443adeb27de47310029ba61f
Nkurunziza Tabazi <sylvain.tabazi@gmail.com>
2025-05-18 09:14:19 -0400

    photo

--- Domain/User.cs
+++ Domain/User.cs
@@ -8,6 +8,7 @@ public class User : IdentityUser<Guid>
     public string? Bio { get; set; }
     public string? ImageUrl { get; set; }
     public ICollection<ActivityAttendee> Activities { get; set; } = [];
+    public ICollection<Photo> Photos { get; set; } = [];
 }
 
 public class UserRole : IdentityRole<Guid>
